18b1eaa95bda537d8741603a7505b6d7587630b4
PabloOsses <pablo.osses2@gmail.com>
2024-09-10 22:00:37 -0300

    avanceregister

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  registerForm: FormGroup;
+  constructor(private formBuilder: FormBuilder,) { 
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    });
 
-  constructor() { }
+  }
 
   ngOnInit() {
+  }
+  onRegister() {
+    if (this.registerForm.valid) {
+      // Lógica para manejar el registro
+      console.log('Formulario de registro válido');
+    }
+  }
+
+  goToLogin() {
+    // Navegación hacia la vista de login
   }
 
 }
